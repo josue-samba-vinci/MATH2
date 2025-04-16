56e04c7110acdf08376739fc6cbf017514cf1f62
Josué  Samba Dianzungu <josue.sambadianzungu@student.vinci.be>
2025-04-16 09:39:56 +0200

    add : ensembles files

---
+++ ensembles/src/Ens1.java
@@ -0,0 +1,50 @@
+public class Ens1 extends EnsembleAbstrait {
+
+	private boolean[] tabB; // e appartient à l'ensemble courant ssi tabE[e.val()] est à true.
+	private int cardinal;
+
+	public Ens1() {
+		//TODO
+	}
+	
+	public boolean estVide() {
+		//TODO
+		return false ;
+	}
+	
+	public Elt unElement() {
+		//TODO
+		return null ;
+	}
+
+	public boolean contient(Elt e) {
+		//TODO
+		return false ;
+	}
+
+	public void ajouter(Elt e) {
+		//TODO
+		
+	}
+
+	public void enlever(Elt e) {
+		//TODO
+		
+	}
+
+	public int cardinal() {
+		//TODO
+		return 0 ;
+	}
+
+	public void complementer() {
+		//TODO
+		
+	}
+
+	public String toString() {
+		// TODO
+		return null;
+	}
+	
+}
---
+++ ensembles/src/Ens2.java
@@ -0,0 +1,51 @@
+public class Ens2 extends EnsembleAbstrait {
+
+	private Elt[] elements; // contient les elements de l'ensemble. Il ne peut pas y avoir de doublon.
+	private int cardinal;
+
+	public Ens2() {
+		//TODO
+		
+	}
+
+	public boolean estVide() {
+		//TODO
+		return false ;
+	}
+	
+	public Elt unElement() {
+		//TODO
+		return null ;
+	}
+
+	public boolean contient(Elt e) {
+		//TODO
+		return false ;
+	}
+
+	public void ajouter(Elt e) {
+		//TODO
+		
+	}
+
+	public void enlever(Elt e) {
+		//TODO
+		
+	}
+
+	public int cardinal() {
+		//TODO
+		return 0;
+	}
+
+	public void complementer() {
+		//TODO;
+		
+	}
+
+	public String toString() {
+		//TODO
+		return null ;
+	}
+
+}
